0400d1e5acb0e099b15ab5cc5280215cb7c7e4b3
AJ ONeal <awesome@coolaj86.com>
2015-08-26 15:06:06 -0400

    got a working template, finally

--- haproxy.example.com/app.js
+++ haproxy.example.com/app.js
@@ -1,35 +1,81 @@
 'use strict';
 
-module.exports.create = function () {
+var PromiseA = require('bluebird');
+
+module.exports.create = function (deps) {
+  var bodyParser = require('body-parser');
+  var Mustache = require('mustache');
   var app = require('express')();
+  var devstore = deps.devstore;
+  var restartHaproxyPath = '/usr/local/bin/restart-haproxy';
 
-  require('child_process').exec("/usr/local/bin/restart-haproxy", function (err, stdout, stderr) {
-
-    console.log('haproxy restarted');
-  });
-
-  app.use(function (req, res) {
+  app.use('/', bodyParser.json({ limit: 1024 * 1024 }));
+  app.use('/', function (req, res) {
     var fs = require('fs');
     var ip = req.connection.remoteAddress;
-    console.log(ip);
 
-    var conf = fs.readFileSync('/etc/haproxy/tpl.cfg', 'utf8');
-    fs.writeFileSync('/etc/haproxy/haproxy.demo.cfg', conf.replace(/{{IP}}/g, ip), 'utf8');
+    // TODO needs validation
+    var device = req.body.device||'none';
+    // TODO needs validation
+    var domains = req.body.domains||[];
 
-    require('child_process').exec("/usr/local/bin/restart-haproxy", function (err, stdout, stderr) {
-      console.log('haproxy started');
+    var promises = domains.map(function (domain) {
+      return devstore.Domains.upsert(domain, { device: device });
+    });
 
-      if (err) {
-        console.error(err);
-      }
-      if (stdout) {
-        console.error(stdout);
-      }
-      if (stderr) {
-        console.error(stderr);
-      }
+    promises.push(devstore.Devices.upsert(device, { name: device, ip: ip }));
+    console.log('[remote client]', device, ip, domains);
 
-      res.send('complete');
+    PromiseA.all(promises).then(function () {
+      devstore.sql.all('SELECT * FROM devices JOIN domains ON devices.name = domains.device', function (err, rows) {
+        if (err) {
+          console.error('[ERROR] sql.get');
+          res.send({ error: { message: "bad query" } });
+          return;
+        }
+
+        //var tplPath = '/etc/haproxy/tpl.cfg';
+        var tplPath = '/etc/haproxy/haproxy.demo.tpl.cfg';
+        //var outPath = '/tmp/haproxy.demo.cfg';
+        var outPath = '/etc/haproxy/haproxy.demo.cfg';
+        var tpl = fs.readFileSync(tplPath, 'utf8');
+        var view = { domains: [], domainsMap: {}, devices: [], devicesMap: {} };
+
+        rows.forEach(function (row) {
+          var dev = view.devicesMap[row.device];
+          var domain = view.domainsMap[row.domain];
+
+          if (!dev) {
+            dev = view.devicesMap[row.device] = row;
+            view.devices.push({ device: row.name || row.device, ip: row.ip });
+          }
+          if (!domain) {
+            domain = view.domainsMap[row.domain] = row;
+            view.domains.push({ domain: row.domain, device: row.device });
+          }
+        });
+
+        var output = Mustache.render(tpl, view);
+
+        // TODO clusterize the write + haproxy restart
+        fs.writeFileSync(outPath, output, 'utf8');
+
+        require('child_process').exec(restartHaproxyPath, function (err, stdout, stderr) {
+          console.log('haproxy started');
+
+          if (err) {
+            console.error(err);
+          }
+          if (stdout) {
+            console.error(stdout);
+          }
+          if (stderr) {
+            console.error(stderr);
+          }
+
+          res.send({ message: '[fake / test] complete' });
+        });
+      });
     });
   });
 
--- haproxy.example.com/serve.js
+++ haproxy.example.com/serve.js
@@ -1,10 +1,105 @@
 'use strict';
+var config = require('./config');
+//var path = require('path');
+var cluster = require('cluster');
+// Always use at least 1 master and 2 workers
+var numCores = Math.max(2, require('os').cpus().length);
+var numWorkers = 0;
+var sqlClient;
 
-var http = require('http');
-var server = http.createServer();
+function spawnWorkers() {
+  // Fork workers
+  while (numWorkers < numCores) {
+    cluster.fork();
+    numWorkers += 1;
+  }
+}
 
-server.on('request', require('./app').create());
+function createClient() {
+  var sqlite3 = require('sqlite3-cluster');
+  var wrap = require('./lib/dbwrap');
+  var dir = [
+    { tablename: 'devices'
+    , idname: 'name'
+    , indices: ['ip']
+    }
+  , { tablename: 'domains'
+    , idname: 'domain'
+    //, relations: [{ tablename: 'devices', id: 'hashid', fk: 'devices.device' }]
+    , indices: ['device']
+    //, immutable: false
+    }
+  ];
 
-server.listen(5080, '10.8.0.1', function () {
-  console.log(server.address());
+  var promise = sqlite3.create({
+      filename: config.sqlcipher.filename
+    , verbose: true
+
+    , standalone: (1 === numCores)
+    , serve: cluster.isMaster
+    //, serve: (numCores > 1) && cluster.isMaster
+    , connect: cluster.isWorker
+    //, connect: (numCores > 1) && cluster.isWorker
+
+    , bits: config.sqlcipher.bits
+  });
+
+  return promise.then(function (db) {
+    return db.init({
+      key: config.sqlcipher.key
+    , bits: config.sqlcipher.bits
+    });
+  }).then(function (db) {
+    return wrap.wrap(db, dir);
+  });
+}
+
+function serve(devdb) {
+  var http = require('http');
+  // TODO
+  //var https = require('https');
+  var server = http.createServer();
+
+  server.on('request', require('./app').create({ devstore: devdb }));
+
+  server.listen(5080, '10.8.0.1', function () {
+    console.log(server.address());
+  });
+}
+
+if (cluster.isMaster) {
+  console.log('[MASTER]', require('os').cpus(), 'cpus');
+
+  spawnWorkers();
+
+  cluster.on('exit', function(worker, code, signal) {
+    numWorkers -= 1;
+    console.log('worker ' + worker.process.pid + ' died with ' + code + ':' + signal);
+    spawnWorkers();
+  });
+
+  var restartHaproxyPath = '/usr/local/bin/restart-haproxy';
+  require('child_process').exec(restartHaproxyPath, function (err, stdout, stderr) {
+    console.log('[Log] haproxy restarted');
+    console.error('[Error]', err, stdout, stderr);
+  });
+}
+
+sqlClient = createClient();
+
+if (cluster.isWorker) {
+  console.log('cluster.worker.id', cluster.worker.id);
+
+  sqlClient.then(function (devdb) {
+    serve(devdb);
+  });
+}
+
+// The native Promise implementation ignores errors because... dumbness???
+process.on('unhandledPromiseRejection', function (err) {
+  console.error('Unhandled Promise Rejection');
+  console.error(err);
+  console.error(err.stack);
+
+  process.exit(1);
 });
